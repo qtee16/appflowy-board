d342c02ad132586e567a56d5571368a12f562cc7
Dong Minh Thai <thai.dm279@gmail.com>
2024-03-13 10:35:34 +0700

    Update reorder_flex.dart scrollbar duplicate

--- lib/src/widgets/reorder_flex/reorder_flex.dart
+++ lib/src/widgets/reorder_flex/reorder_flex.dart
@@ -598,10 +598,8 @@ class ReorderFlexState extends State<ReorderFlex>
         PrimaryScrollController.of(context) == null) {
       return child;
     } else {
-      return Scrollbar(
-        controller: _scrollController,
-        thickness: 5,
-        child: SingleChildScrollView(
+      if (widget.config.direction == Axis.horizontal) {
+        return SingleChildScrollView(
           physics: ClampingScrollPhysics(),
           scrollDirection: widget.config.direction,
           controller: _scrollController,
@@ -609,8 +607,22 @@ class ReorderFlexState extends State<ReorderFlex>
             margin: EdgeInsets.only(right: 4),
             child: child
           ),
-        )
-      );
+        );
+      } else {
+        return Scrollbar(
+          controller: _scrollController,
+          thickness: 5,
+          child: SingleChildScrollView(
+            physics: ClampingScrollPhysics(),
+            scrollDirection: widget.config.direction,
+            controller: _scrollController,
+            child: Container(
+              margin: EdgeInsets.only(right: 4),
+              child: child
+            ),
+          )
+        );
+      }
     }
   }
 
